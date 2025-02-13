8384cbb5878e8e9af8c18ff1b613c360b2ecce41
Orion Hess <orionhess@gmail.com>
2025-02-12 18:21:36 -0800

    Initial impls, not tested

--- src/main/java/cpsc326/SimpleParser.java
+++ src/main/java/cpsc326/SimpleParser.java
@@ -113,9 +113,11 @@ public class SimpleParser {
     while (!match(TokenType.EOS)) {
       // Struct definition
       if (match(TokenType.STRUCT)) {
-        advance();
         structDef();
-
+      }
+      // Function definition
+      else {
+        funDef();
       }
     }
   }
@@ -124,195 +126,393 @@ public class SimpleParser {
    * Checks for a valid struct definition.
    */ 
   private void structDef() {
-    // TODO: implement this function
+    // First already checked
+    advance();
+    eat(TokenType.ID, "Expected ID");
+    eat(TokenType.LBRACE, "Expected LBRACE");
+    fields();
+    eat(TokenType.RBRACE, "Expected RBRACE");
   }
 
   /**
    * Checks for valid fields
    */
   private void fields() {
-
+    if (match(TokenType.ID)) {
+      field();
+      while (match(TokenType.COMMA)) {
+        advance();
+        field();
+      }
+    }
   }
 
   /**
    * Checks for a valid field
    */
   private void field() {
-
+    // ID already checked
+    advance();
+    eat(TokenType.COLON, "Expected COLON");
+    dataType();
   }
 
   /**
    * Checks for a valid function definition.
    */ 
   private void funDef() {
-    // TODO: implement this function
+    returnType();
+    eat(TokenType.ID, "Expected ID");
+    eat(TokenType.LPAREN, "Expected LPAREN");
+    params();
+    eat(TokenType.RPAREN, "Expected RPAREN");
+    block();
   }
 
   /**
    * Checks for a valid block
    */
   private void block() {
-
+    eat(TokenType.LBRACE, "Expected LBRACE");
+    while (!match(TokenType.RBRACE)) {
+      stmt();
+    }
+    // Hit an RBRACE
+    advance();
   }
 
   /**
    * Checks for a valid returnType
    */
   private void returnType() {
-
+    if (match(TokenType.VOID_TYPE)) {
+      advance();
+    } else {
+      dataType();
+    }
   }
 
   /**
    * Checks for valid params
    */
   private void params() {
-
+    if (match(TokenType.ID)) {
+      param();
+      while (match(TokenType.COMMA)) {
+        advance();
+        param();
+      }
+    }
   }
 
   /**
    * Checks for a valid param
    */
   private void param() {
-
+    eat(TokenType.ID, "Expected ID");
+    eat(TokenType.COLON, "Expected COLON");
+    dataType();
   }
 
   /**
    * Checks for a valid dataType
    */
   private void dataType() {
-
+    if (match(TokenType.LBRACKET)) {
+      advance();
+      if (match(TokenType.ID)) {
+        advance();
+      } else {
+        baseType();
+      }
+      eat(TokenType.RBRACKET, "Expected RBRACKET");
+    } else if (match(TokenType.ID)) {
+      advance();
+    } else {
+      baseType();
+    }
   }
 
   /**
-   * Checks for a valid blockType
+   * Checks for a valid baseType
    */
-  private void blockType() {
-
+  private void baseType() {
+    if (matchAny(List.of(TokenType.INT_TYPE, TokenType.DOUBLE_TYPE, TokenType.STRING_TYPE, TokenType.BOOL_TYPE))) {
+      advance();
+    } else {
+      error("Expected one of INT_TYPE, DOUBLE_TYPE, STRING_TYPE, BOOL_TYPE");
+    }
   }
 
   /**
    * Checks for a valid stmt
    */
   private void stmt() {
-
+    switch (currToken.tokenType) {
+      case TokenType.VAR:
+        varStmt();
+        break;
+      case TokenType.WHILE:
+        whileStmt();
+        break;
+      case TokenType.IF:
+        ifStmt();
+        break;
+      case TokenType.FOR:
+        forStmt();
+        break;
+      case TokenType.RETURN:
+        returnStmt();
+        break;
+      case TokenType.ID:
+        advance();
+        if (match(TokenType.LPAREN)) {
+          funCall();
+        } else {
+          assignStmt();
+        }
+        break;
+      default:
+        error("Expected any of VAR, WHILE, IF, FOR, RETURN, ID");
+    }
   }
 
   /**
    * Checks for a valid varStmt
    */
   private void varStmt() {
-
+    // Var already checked
+    advance();
+    eat(TokenType.ID, "Expected ID");
+    if (match(TokenType.ASSIGN)) {
+      varInit();
+    } else {
+      varType();
+      if (match(TokenType.ASSIGN)) {
+        varInit();
+      }
+    }
   }
 
   /**
    * Checks for a valid varType
    */
   private void varType() {
-
+    eat(TokenType.COLON, "Expected COLON");
+    dataType();
   }
 
   /**
    * Checks for a valid varInit
    */
   private void varInit() {
-
+    // ASSIGN already checked
+    advance();
+    expr();
   }
 
   /**
    * Checks for a valid whileStmt
    */
   private void whileStmt() {
-
+    // WHILE already checked
+    advance();
+    expr();
+    block();
   }
 
   /**
    * Checks for a valid ifStmt
    */
   private void ifStmt() {
-
+    // IF already checked
+    advance();
+    expr();
+    block();
+    if (match(TokenType.ELSE)) {
+      advance();
+      if (match(TokenType.IF)) {
+        ifStmt();
+      } else {
+        block();
+      }
+    }
   }
 
   /**
    * Checks for a valid forStmt
    */
   private void forStmt() {
-
+    // FOR already checked
+    advance();
+    eat(TokenType.ID, "Expected ID");
+    eat(TokenType.FROM, "Expected FROM");
+    expr();
+    eat(TokenType.TO, "Expected TO");
+    expr();
+    block();
   }
 
   /**
    * Checks for a valid returnStmt
    */
   private void returnStmt() {
-
+    // RETURN already checked
+    advance();
+    expr();
   }
 
   /**
    * Checks for a valid assignStmt
    */
   private void assignStmt() {
-
+    lvalue();
+    eat(TokenType.ASSIGN, "Expected ASSIGN");
+    expr();
   }
 
   /**
    * Checks for a valid lvalue
    */
   private void lvalue() {
-
+    // ID already checked and advanced (See stmt())
+    if (match(TokenType.LBRACKET)) {
+      advance();
+      expr();
+      eat(TokenType.RBRACKET, "Expected RBRACKET");
+    }
+    while (match(TokenType.DOT)) {
+      advance();
+      eat(TokenType.ID, "Expected ID");
+      if (match(TokenType.LBRACKET)) {
+        advance();
+        expr();
+        eat(TokenType.RBRACKET, "Expected RBRACKET");
+      }
+    }
   }
 
   /**
    * Checks for a valid funCall
    */
   private void funCall() {
-
+    // ID already checked and advanced (See stmt())
+    eat(TokenType.LPAREN, "Expected LPAREN");
+    args();
+    eat(TokenType.RPAREN, "Expected RPAREN");
   }
 
   /**
    * Checks for a valid args
    */
   private void args() {
-
+    // If there aren't args, there will be an RPAREN
+    if  (!match(TokenType.RPAREN)) {
+      expr();
+    }
+    while (match(TokenType.COMMA)) {
+      advance();
+      expr();
+    }
   }
 
   /**
    * Checks for a valid expr
    */
   private void expr() {
-
+    if (match(TokenType.NOT)) {
+      advance();
+      expr();
+    } else if (match(TokenType.LPAREN)) {
+      advance();
+      args();
+      eat(TokenType.RPAREN, "Expected RPAREN");
+    } else {
+      rvalue();
+    }
+    if (isBinOp()) {
+      binOp();
+      expr();
+    }
   }
 
   /**
    * Checks for a valid binOp
    */
   private void binOp() {
-
+    // isBinOp() already checked
+    advance();
   }
 
   /**
    * Checks for a valid rvalue
    */
   private void rvalue() {
-
+    if (isLiteral()) {
+      literal();
+    } else if (match(TokenType.NEW)) {
+      newRvalue();
+    } else if (match(TokenType.ID)) {
+      // Must look further ahead
+      advance();
+      if (match(TokenType.LPAREN)) {
+        funCall();
+      } else {
+        varRvalue();
+      }
+    } else {
+      error("Expected one of LITERAL, NEW, ID");
+    }
   }
 
   /**
    * Checks for a valid newRvalue
    */
   private void newRvalue() {
-
+    // NEW already checked
+    advance();
+    if (match(TokenType.ID)) {
+      advance();
+    } else {
+      baseType();
+    }
+    if (match(TokenType.LPAREN)) {
+      advance();
+      args();
+      eat(TokenType.RPAREN, "Expected RPAREN");
+    } else {
+      eat(TokenType.RBRACKET, " Expected RBRACKET");
+      expr();
+      eat(TokenType.RBRACKET, "Expected RBRACKET");
+    }
   }
 
   /**
    * Checks for a valid literal
    */
   private void literal() {
-
+    // isLiteral() already checked
+    advance();
   }
 
   /**
    * Checks for a valid varRvalue
    */
   private void varRvalue() {
-
+    // ID already checked and advanced
+    if (match(TokenType.LBRACKET)) {
+      advance();
+      expr();
+      eat(TokenType.RBRACKET, "Expected RBRACKET");
+    }
+    while (match(TokenType.DOT)) {
+      advance();
+      eat(TokenType.ID, "Expected ID");
+      if (match(TokenType.LBRACKET)) {
+        advance();
+        expr();
+        eat(TokenType.RBRACKET, "Expected RBRACKET");
+      }
+    }
   }
 }
