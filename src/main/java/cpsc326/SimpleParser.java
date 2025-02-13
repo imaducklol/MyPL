7ff875876845ad739bccc9ed0e627a445aa07725
Orion Hess <orionhess@gmail.com>
2025-02-12 17:04:43 -0800

    Function decls

--- src/main/java/cpsc326/SimpleParser.java
+++ src/main/java/cpsc326/SimpleParser.java
@@ -110,7 +110,14 @@ public class SimpleParser {
    * Checks for a valid program.
    */ 
   private void program() {
-    // TODO: implement this function
+    while (!match(TokenType.EOS)) {
+      // Struct definition
+      if (match(TokenType.STRUCT)) {
+        advance();
+        structDef();
+
+      }
+    }
   }
 
   /**
@@ -121,13 +128,191 @@ public class SimpleParser {
   }
 
   /**
-   * checks for a valid function definition.
+   * Checks for valid fields
+   */
+  private void fields() {
+
+  }
+
+  /**
+   * Checks for a valid field
+   */
+  private void field() {
+
+  }
+
+  /**
+   * Checks for a valid function definition.
    */ 
   private void funDef() {
     // TODO: implement this function
   }
 
-  // ... and so on ...   
-  // TODO: implement the rest of the recursive descent functions 
-  
+  /**
+   * Checks for a valid block
+   */
+  private void block() {
+
+  }
+
+  /**
+   * Checks for a valid returnType
+   */
+  private void returnType() {
+
+  }
+
+  /**
+   * Checks for valid params
+   */
+  private void params() {
+
+  }
+
+  /**
+   * Checks for a valid param
+   */
+  private void param() {
+
+  }
+
+  /**
+   * Checks for a valid dataType
+   */
+  private void dataType() {
+
+  }
+
+  /**
+   * Checks for a valid blockType
+   */
+  private void blockType() {
+
+  }
+
+  /**
+   * Checks for a valid stmt
+   */
+  private void stmt() {
+
+  }
+
+  /**
+   * Checks for a valid varStmt
+   */
+  private void varStmt() {
+
+  }
+
+  /**
+   * Checks for a valid varType
+   */
+  private void varType() {
+
+  }
+
+  /**
+   * Checks for a valid varInit
+   */
+  private void varInit() {
+
+  }
+
+  /**
+   * Checks for a valid whileStmt
+   */
+  private void whileStmt() {
+
+  }
+
+  /**
+   * Checks for a valid ifStmt
+   */
+  private void ifStmt() {
+
+  }
+
+  /**
+   * Checks for a valid forStmt
+   */
+  private void forStmt() {
+
+  }
+
+  /**
+   * Checks for a valid returnStmt
+   */
+  private void returnStmt() {
+
+  }
+
+  /**
+   * Checks for a valid assignStmt
+   */
+  private void assignStmt() {
+
+  }
+
+  /**
+   * Checks for a valid lvalue
+   */
+  private void lvalue() {
+
+  }
+
+  /**
+   * Checks for a valid funCall
+   */
+  private void funCall() {
+
+  }
+
+  /**
+   * Checks for a valid args
+   */
+  private void args() {
+
+  }
+
+  /**
+   * Checks for a valid expr
+   */
+  private void expr() {
+
+  }
+
+  /**
+   * Checks for a valid binOp
+   */
+  private void binOp() {
+
+  }
+
+  /**
+   * Checks for a valid rvalue
+   */
+  private void rvalue() {
+
+  }
+
+  /**
+   * Checks for a valid newRvalue
+   */
+  private void newRvalue() {
+
+  }
+
+  /**
+   * Checks for a valid literal
+   */
+  private void literal() {
+
+  }
+
+  /**
+   * Checks for a valid varRvalue
+   */
+  private void varRvalue() {
+
+  }
 }
