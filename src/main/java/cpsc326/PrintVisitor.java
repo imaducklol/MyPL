4f2ae278907c5ddffb131896a524a45030768aad
Orion Hess <orionhess@gmail.com>
2025-02-27 02:31:08 -0800

    PrintVisitor stubbed

--- src/main/java/cpsc326/PrintVisitor.java
+++ src/main/java/cpsc326/PrintVisitor.java
@@ -64,5 +64,63 @@ public class PrintVisitor implements Visitor {
   // Use the above helper functions to write, deal with indentation,
   // and print newlines as part of your visit functions.
 
+  // general AST classes
+  public void visit(FunDef node) {
+
+  }
+  public void visit(StructDef node) {
+
+  }
+  public void visit(DataType node) {
+
+  }
+  public void visit(VarDef node) {
+
+  }
+  // statements
+  public void visit(ReturnStmt node) {
+
+  }
+  public void visit(VarStmt node) {
+
+  }
+  public void visit(AssignStmt node) {
+
+  }
+  public void visit(WhileStmt node) {
+
+  }
+  public void visit(ForStmt node) {
+
+  }
+  public void visit(IfStmt node) {
+
+  }
+  // expressions
+  public void visit(BasicExpr node) {
+
+  }
+  public void visit(UnaryExpr node) {
+
+  }
+  public void visit(BinaryExpr node) {
+
+  }
+  public void visit(CallRValue node) {
+
+  }
+  public void visit(SimpleRValue node) {
+
+  }
+  public void visit(NewStructRValue node) {
+
+  }
+  public void visit(NewArrayRValue node) {
+
+  }
+  public void visit(VarRValue node) {
+
+  }
+
   
 }
