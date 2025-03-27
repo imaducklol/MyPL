b7d9be28748bde3e240ab25b7d6b45f3b0995358
Orion Hess <orionhess@gmail.com>
2025-03-27 14:16:48 -0700

    Implemented and tested Prog1 & Prog2

--- src/main/java/cpsc326/HW5Prog1.java
+++ src/main/java/cpsc326/HW5Prog1.java
@@ -13,8 +13,8 @@ public class HW5Prog1 {
   // Implement the following MyPL program: 
   // 
   // bool is_prime(n: int) {
-  //   var m: int = n / 2
-  //   var v: int = 2
+  //   var m: int = n / 2 // 2
+  //   var v: int = 2     // 3
   //   while v <= m {
   //     var r: int = n / v
   //     var p: int = r * v
@@ -48,7 +48,72 @@ public class HW5Prog1 {
     // TODO: Add the instructions for main (m) and is_prime (p)
     //       templates to implement the corresponding main and
     //       is_prime functions above.
-    
+    m.add(VMInstr.PUSH("Please enter integer values to sum (prime to quit)\n"));
+    m.add(VMInstr.WRITE());
+    m.add(VMInstr.PUSH(0));
+    m.add(VMInstr.STORE(0)); // sum
+
+    m.add(VMInstr.NOP()); // start while loop
+    m.add(VMInstr.PUSH("Enter an int: "));
+    m.add(VMInstr.WRITE());
+    m.add(VMInstr.READ());
+    m.add(VMInstr.TOINT());
+    m.add(VMInstr.STORE(1)); // val
+    m.add(VMInstr.LOAD(1)); // startif
+    m.add(VMInstr.CALL("is_prime"));
+    m.add(VMInstr.JMPF(24));
+    m.add(VMInstr.PUSH("The sum is: "));
+    m.add(VMInstr.LOAD(0));
+    m.add(VMInstr.TOSTR());
+    m.add(VMInstr.PUSH("\n"));
+    m.add(VMInstr.ADD());
+    m.add(VMInstr.ADD());
+    m.add(VMInstr.WRITE());
+    m.add(VMInstr.PUSH("Goodbye!\n"));
+    m.add(VMInstr.WRITE());
+    m.add(VMInstr.PUSH(VM.NULL));
+    m.add(VMInstr.RET()); // endif
+    m.add(VMInstr.LOAD(0));
+    m.add(VMInstr.LOAD(1));
+    m.add(VMInstr.ADD());
+    m.add(VMInstr.STORE(0));
+    m.add(VMInstr.JMP(4));
+
+
+
+
+    p.add(VMInstr.STORE(0)); // store n
+    p.add(VMInstr.LOAD(0)); // calc m
+    p.add(VMInstr.PUSH(2));
+    p.add(VMInstr.DIV());
+    p.add(VMInstr.STORE(1)); // m
+    p.add(VMInstr.PUSH(2));
+    p.add(VMInstr.STORE(2)); // v
+    p.add(VMInstr.LOAD(2)); // loop
+    p.add(VMInstr.LOAD(1));
+    p.add(VMInstr.CMPLE());
+    p.add(VMInstr.JMPF(30));
+    p.add(VMInstr.LOAD(0)); // inside loop
+    p.add(VMInstr.LOAD(2));
+    p.add(VMInstr.DIV());
+    p.add(VMInstr.STORE(3));
+    p.add(VMInstr.LOAD(3));
+    p.add(VMInstr.LOAD(2));
+    p.add(VMInstr.MUL());
+    p.add(VMInstr.STORE(4));
+    p.add(VMInstr.LOAD(4));
+    p.add(VMInstr.LOAD(0));
+    p.add(VMInstr.CMPEQ());
+    p.add(VMInstr.JMPF(25));
+    p.add(VMInstr.PUSH(false));
+    p.add(VMInstr.RET());
+    p.add(VMInstr.LOAD(2));
+    p.add(VMInstr.PUSH(1));
+    p.add(VMInstr.ADD());
+    p.add(VMInstr.STORE(2));
+    p.add(VMInstr.JMP(7));
+    p.add(VMInstr.PUSH(true));
+    p.add(VMInstr.RET());
 
     // create the vm: 
     VM vm = new VM();
--- src/main/java/cpsc326/HW5Prog2.java
+++ src/main/java/cpsc326/HW5Prog2.java
@@ -36,6 +36,102 @@ public class HW5Prog2 {
     // TODO: Add the instructions to the main (m) template to
     //       implement the above main function.
 
+    // create struct array and store it to 0
+    m.add(VMInstr.PUSH(2));
+    m.add(VMInstr.ALLOCA());
+    m.add(VMInstr.STORE(0));
+
+    // set up array assignment
+    m.add(VMInstr.LOAD(0));
+    m.add(VMInstr.PUSH(0));
+
+    // create first struct
+    m.add(VMInstr.ALLOCS());
+    m.add(VMInstr.DUP());
+    m.add(VMInstr.PUSH("a"));
+    m.add(VMInstr.SETF("name"));
+    m.add(VMInstr.DUP());
+    m.add(VMInstr.PUSH(10));
+    m.add(VMInstr.SETF("wins"));
+    m.add(VMInstr.DUP());
+    m.add(VMInstr.PUSH(20));
+    m.add(VMInstr.SETF("games"));
+
+    // assign first struct
+    m.add(VMInstr.SETI());
+
+    // set up array assignment
+    m.add(VMInstr.LOAD(0));
+    m.add(VMInstr.PUSH(1));
+
+    // create second struct
+    m.add(VMInstr.ALLOCS());
+    m.add(VMInstr.DUP());
+    m.add(VMInstr.PUSH("B"));
+    m.add(VMInstr.SETF("name"));
+    m.add(VMInstr.DUP());
+    m.add(VMInstr.PUSH(18));
+    m.add(VMInstr.SETF("wins"));
+    m.add(VMInstr.DUP());
+    m.add(VMInstr.PUSH(20));
+    m.add(VMInstr.SETF("games"));
+
+    // assign second struct
+    m.add(VMInstr.SETI());
+
+    // sum assignment
+    m.add(VMInstr.PUSH(0.0));
+    m.add(VMInstr.STORE(1));
+
+    // first math
+    m.add(VMInstr.LOAD(1));
+
+    m.add(VMInstr.LOAD(0));
+    m.add(VMInstr.PUSH(0));
+    m.add(VMInstr.GETI());
+    m.add(VMInstr.GETF("wins"));
+    m.add(VMInstr.TODBL());
+    m.add(VMInstr.LOAD(0));
+    m.add(VMInstr.PUSH(0));
+    m.add(VMInstr.GETI());
+    m.add(VMInstr.GETF("games"));
+    m.add(VMInstr.TODBL());
+    m.add(VMInstr.DIV());
+
+    m.add(VMInstr.ADD());
+    m.add(VMInstr.STORE(1));
+
+    // second math
+    m.add(VMInstr.LOAD(1));
+
+    m.add(VMInstr.LOAD(0));
+    m.add(VMInstr.PUSH(1));
+    m.add(VMInstr.GETI());
+    m.add(VMInstr.GETF("wins"));
+    m.add(VMInstr.TODBL());
+    m.add(VMInstr.LOAD(0));
+    m.add(VMInstr.PUSH(1));
+    m.add(VMInstr.GETI());
+    m.add(VMInstr.GETF("games"));
+    m.add(VMInstr.TODBL());
+
+    m.add(VMInstr.DIV());
+
+    m.add(VMInstr.ADD());
+    m.add(VMInstr.STORE(1));
+
+    m.add(VMInstr.PUSH("The average win percentage is: "));
+    m.add(VMInstr.WRITE());
+
+    m.add(VMInstr.LOAD(1));
+    m.add(VMInstr.PUSH(2));
+    m.add(VMInstr.TODBL());
+    m.add(VMInstr.DIV());
+    m.add(VMInstr.WRITE());
+
+    m.add(VMInstr.PUSH("\n"));
+    m.add(VMInstr.WRITE());
+
     // create the vm: 
     VM vm = new VM();
     // add the frame: 
